ad344c4d0a8ebd8847faa2cec777818420ae98fd
OLiang24 <68525467+OLiang24@users.noreply.github.com>
2020-10-10 23:31:31 +0700

    cinema

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+      <a class="navbar-brand- mr-auto ">OLiang Movie </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navContent"
+        aria-controls="navContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link :to="{name: 'MovieList'}" class="nav-link">รายการภาพยนต์</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{name: 'CreateMovie'}" class="nav-link">เพิ่มรายการภาพยนต์</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+import { store } from "./store/store";
+export default {
+  store
+};
+</script>
